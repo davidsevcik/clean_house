5aa256bea44d00bc215d70cb8bb5f4dfe8402f35
David Sevcik <david.sevcik@gmail.com>
2012-04-30 19:08:48 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CleanHouse::Application.config.secret_token = '4891c4b55d06ae1f9cbdb600e977a3369d8549050bea6c2f04ca98495ca7ca09d7389034f3d57056c698e847415a223a78cfd45ecce75ad658a93517bb23fa2b'
